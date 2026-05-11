5b1d02329631bcd9895ca1f5df50e768eb38db9b
Francois <Francois@EV-DEV-P010.BEAUVOIR.EVRY.FR>
2026-05-11 11:00:03 +0200

    fil-rouge-francois

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,25 @@
 const burger = document.getElementById("burger");
 const menu = document.getElementById("menu");
 
-burger.addEventListener("click", () => {
-  menu.classList.toggle("active");
+if (burger && menu) {
+  burger.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
+}
+
+
+// modal recherceh 
+
+
+const searchBtn = document.getElementById("searchBtn");
+const searchInput = document.getElementById("searchInput");
+
+searchBtn.addEventListener("click", () => {
+    const recherche = searchInput.value.trim();
+
+    if (recherche !== "") {
+        console.log("Recherche :", recherche);
+    } else {
+        console.log("Champ vide");
+    }
 });

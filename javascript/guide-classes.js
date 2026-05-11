d3f521eeb9ebcaf9c46b595fcd7eb719767f3d9c
Francois <Francois@EV-DEV-P010.BEAUVOIR.EVRY.FR>
2026-05-11 16:27:03 +0200

    fil-rouge-francois

---
+++ javascript/guide-classes.js
@@ -0,0 +1,15 @@
+function openModal(className) {
+    document.getElementById("modal-title").innerText = className;
+    document.getElementById("modal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
+
+window.onclick = function(event) {
+    const modal = document.getElementById("modal");
+    if (event.target === modal) {
+        closeModal();
+    }
+}
